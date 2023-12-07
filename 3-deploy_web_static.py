c1620e6a4091994873496dceaca7f08efcbaba2a
MwendeHarriet <mwendeharriet@gmail.com>
2023-12-07 12:37:54 -0800

    wwwwww

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-"""This module creates and distributes an archive to your web servers
-    using the function deploy"""
+"""This Fabric script, inspired by the concepts in the file
+    2-do_deploy_web_static.py is designed to generate and
+    distribute a compressed archive to deploy"""
 
+from fabric.api import env, local, put, run
 from datetime import datetime
-from fabric.api import local, run, env, put
-from os.path import exists, isdir  # Import isdir here
+from os.path import exists, isdir
+
 env.hosts = ['54.237.85.149', '52.91.127.145']
 
+
 def do_pack():
-    """generates a tgz archive"""
+    """Generates a TGZ archive of the web_static directory."""
     try:
         date = datetime.now().strftime("%Y%m%d%H%M%S")
         if isdir("versions") is False:
@@ -16,12 +19,12 @@ def do_pack():
         file_name = "versions/web_static_{}.tgz".format(date)
         local("tar -cvzf {} web_static".format(file_name))
         return file_name
-    except:
+    except Exception:
         return None
 
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
+    """Distributes the specified archive to the web servers."""
     if exists(archive_path) is False:
         return False
     try:
@@ -37,12 +40,12 @@ def do_deploy(archive_path):
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except:
+    except Exception:
         return False
 
 
 def deploy():
-    """creates and distributes an archive to the web servers"""
+    """Creates and distributes an archive to the web servers."""
     archive_path = do_pack()
     if archive_path is None:
         return False
