5ca9cb155d97c15efd71c40fef37712d4c3fbbc2
MwendeHarriet <mwendeharriet@gmail.com>
2023-12-27 03:21:44 -0800

    task 6

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""This script starts flask application"""
+
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def main():
+    """Function returns hello hbnb"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Function returns hello hbnb"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def C_time(text):
+    """Function returns hello hbnb"""
+    x = text.replace('_', ' ')
+    st = "C " + x
+    return st
+
+
+@app.route('/python', defaults={'text': "is_cool"}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def py_time(text):
+    """Fuction returns hello hbnb"""
+    x = text.replace('_', ' ')
+    st = "Python " + x
+    return st
+
+
+@app.route('/number/<int:Number>', strict_slashes=False)
+def N_time(Number):
+    """Function returns hello hbnb"""
+    st = str(Number) + " is a number"
+    return st
+
+
+@app.route('/number_template/<int:Number>', strict_slashes=False)
+def N_r_time(Number):
+    """Function returns hello hbnb"""
+    return render_template('5-number.html', num=Number)
+
+
+@app.route('/number_odd_or_even/<int:Number>', strict_slashes=False)
+def N_oe_time(Number):
+    """Function returns hello hbnb"""
+    if Number % 2 == 0:
+        oe = "even"
+    else:
+        oe = "odd"
+    return render_template('6-number_odd_or_even.html', num=Number, oe=oe)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
